6fcd4f4184aaa75516f26e803375d9f43326a589
Raihan Arpe <raihanarpe@gmail.com>
2023-02-17 00:51:46 +0700

    fix invinsible element and responsive problem)

--- landing/siswa/siswa.php
+++ landing/siswa/siswa.php
@@ -6,10 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=`device-width`, initial-scale=1.0">
     <title>Document</title>
-    <?php
-
-
-    ?>
     <link rel="stylesheet" href="../../css/siswa/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- bootstrap sekiranya cdnnya down -->
@@ -80,7 +76,9 @@
             </section>
         </div>
     </div>
+    <footer>
 
+    </footer>
 
 
 
