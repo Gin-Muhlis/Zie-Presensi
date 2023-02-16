da37ff300b16bd4a743e22a1cd0a50a6b7fb4a5b
Raihan Arpe <raihanarpe@gmail.com>
2023-02-16 09:18:56 +0700

    siswa page

---
+++ landing/siswa/siswa.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=`device-width`, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/siswa/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- bootstrap sekiranya cdnnya down -->
+    <script>
+        var test = document.createElement("div")
+        test.className = "hidden d-none"
+
+        document.head.appendChild(test)
+        var cssLoaded = window.getComputedStyle(test).display === "none"
+        document.head.removeChild(test)
+
+        if (!cssLoaded) {
+            var link = document.createElement("link");
+
+            link.type = "text/css";
+            link.rel = "stylesheet";
+            link.href = "bootstrap-5.2.3-dist/css/bootstrap.min.css";
+
+            document.head.appendChild(link);
+        }
+    </script>
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <div class="profile">
+            <div class="profile-container">
+                <div class="info-profile">
+                    <p>Nama Lengkap Siswa</p>
+                    <p>NISN</p>
+                </div>
+                <div class="img-container">
+                    <img src="" alt="" srcset="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section>
+        <div class="pilihan">
+            <div class="pilihan-container">
+                <div class="tombol-container-1">
+                    <button>Presensi</button>
+                </div>
+                <div class="tombol-container-2">
+                    <button>Skejul</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+    <!-- JS bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <!-- JS bootstrap sekiranya cdnnya down -->
+    <script>
+        if (typeof($.fn.modal) === 'undefined') {
+            document.write('<script src="bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js"><\/script>')
+        }
+    </script>
+</body>
+
+</html>
